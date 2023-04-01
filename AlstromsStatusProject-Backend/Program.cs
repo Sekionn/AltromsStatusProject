b786aa2da9012a0e9e773dca654b4678956760d6
Sekionn <74351053+Sekionn@users.noreply.github.com>
2023-04-01 19:01:18 +0200

    Update misc

--- AlstromsStatusProject-Backend/Program.cs
+++ AlstromsStatusProject-Backend/Program.cs
@@ -25,8 +25,8 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseCors();
